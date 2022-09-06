82a6a28adea4af66eebba5b68fa55bdd86bb9227
YufaGinaMaryafa <110004619+Yufamaryafa@users.noreply.github.com>
2022-09-06 11:22:16 +0700

    Add files via upload

---
+++ rumus-persegi.php
@@ -0,0 +1,18 @@
+<form action='' method="">
+    <h1> Rumus Luas dan Keliling Persegi </h1>
+    <p>Sisi :</p>
+    <input type="number" name="Sisi" Placeholder="Ex. 5" />
+    <input type="submit" name="proses" value="Hitung Luas & Keliling" />
+</form>
+
+<?php 
+    if ( isset($_POST["proses"]) ){
+        echo "<hr>";
+        $sisi = $_POST["Sisi"];
+        $luas = $sisi * $sisi;
+        $keliling = 4 * $sisi;
+
+        echo "Sisi : $sisi <br>";
+        echo "Luas Persegi : $luas <br>";
+        echo "Keliling Persegi : $keliling <br>";
+    }
