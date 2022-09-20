8b231ee28b68fc766ac63fcc4ba7ea4905281005
YufaGinaMaryafa <110004619+Yufamaryafa@users.noreply.github.com>
2022-09-20 12:43:24 +0700

    Add files via upload

--- input-config.php
+++ input-config.php
@@ -1,7 +1,7 @@
 <?php
-    $mysqli = new mysqli("localhost","root","","siswa_rpl");
+    $mysqli = new mysqli("localhost","root","","siswa_yufa");
     if ($mysqli -> connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
         exit();
     }
-?>            
+?>        
---
+++ input-nilaisiswa.php
@@ -0,0 +1,48 @@
+<?php
+    include('./input-config.php');
+    echo "<a href='input-nilaisiswa-tambah.php'>Tambah Data</a>";
+    echo "<hr>";
+    // Menampilkan data nilai database
+    $no = 1;
+    $tabledata = "";
+    $data = mysqli_query($mysqli," SELECT * FROM siswa_nilai ");
+    while($row = mysqli_fetch_array($data)){
+        $nilai_akhir=($row["kehadiran"]+$row["tugas"]+$row["uts"]+$row["uas"])/4;
+        $tabledata .= "
+            <tr>
+                <td>".$row["nis"]."</td>
+                <td>".$row["namalengkap"]."</td>
+                <td>".$row["kelas"]."</td>
+                <td>".$row["kehadiran"]."</td>
+                <td>".$row["tugas"]."</td>
+                <td>".$row["uts"]."</td>
+                <td>".$row["uas"]."</td>
+                <td>".$nilai_akhir."</td>
+                <td>
+                    <a href='input-nilaisiswa-edit.php?nis=".$row["nis"]."'>Edit</a>
+                    &nbsp;-&nbsp;
+                    <a href='input-nilaisiswa-hapus.php?nis=".$row["nis"]."'
+                    onclick='return confirm(\"Yakin Hapus ?\");'>Hapus</a>
+                </td>
+            </tr>
+        ";
+        $no++;
+    }
+
+    echo "
+        <table cellpadding=5 border=1 cellspacing=0>
+            <tr>
+                <th>NIS</th>
+                <th>Nama Lengkap</th>
+                <th>Kelas</th>
+                <th>Kehadiran</th>
+                <th>Tugas</th>
+                <th>UTS</th>
+                <th>UAS</th>
+                <th>Nilai Akhir</th>
+                <th>Aksi</th>
+            </tr>
+            $tabledata
+        </table>
+    ";
+?>
